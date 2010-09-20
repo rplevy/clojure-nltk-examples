97282ae91e8e9d33c28088be2ff5711fd19bfa72
Rob Levy <rob@voronoi.(none)>
2010-09-20 04:40:48 -0400

    first commit. not much here yet, but it does demonstrate that clojure-nltk is a self-contained dependency in leiningen, which is pretty cool.  the only caveat is that jython 2.5.1 is not in Maven central, so you need to run one mvn install command to add it to a local repo before running lein deps.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-nltk-examples "0.0.1"
+  :description "Examples demonstrating use of clojure-nltk library."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [clojure-nltk "0.0.3"]]
+  :main clojure-nltk-examples.core)
---
+++ src/clojure_nltk_examples/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-nltk-examples.core
+  (:use clojure-nltk.core))
+
+(clojure-nltk.core/init)
+
+;; functions to load examples go here
